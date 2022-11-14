fe2e66fbb1c67b0d00982bf87cbf9737ae2937ba
jacksonohallo <jacksonohallo@gmail.com>
2022-11-14 14:37:05 +0300

    feat: add navbar data

---
+++ src/data/NavbarData.js
@@ -0,0 +1,18 @@
+export const NavbarData = [
+    {
+        name: 'Home',
+        link: '/'
+    },
+    {
+        name: 'Place to stay',
+        link: '/placeToStay'
+    },
+    {
+        name: 'NFTs',
+        link: '/'
+    },
+    {
+        name: 'Community',
+        link: '/'
+    },
+]
