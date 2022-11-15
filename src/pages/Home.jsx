9b02875bdde9b03aaa7b173cb083b2db7a6dc08a
jacksonohallo <jacksonohallo@gmail.com>
2022-11-15 00:21:57 -0800

    test: test fuctionality

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+// import React from "react";
 import HeroSection from "../sections/HeroSection";
 import Inspirations from "../sections/Inspirations";
 import Banner from "../sections/Banner";
